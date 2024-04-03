056689d0ba04f5ffdd5feb1afa1301b370102e7f
howisrian <riansantos.dev@gmail.com>
2024-04-03 12:09:01 -0300

    Adicionando conteudo

---
+++ my-ecommerce-app/src/components/Carousel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Carousel() {
+  return (
+    <div className="carousel">
+      {/* Aqui você colocaria o conteúdo do carrossel, como imagens ou produtos em destaque */}
+      <h2>Carousel of Highlights</h2>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ my-ecommerce-app/src/components/ProductSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductSection() {
+  return (
+    <div className="product-section">
+      {/* Aqui você colocaria os produtos */}
+      <h2>Product Section</h2>
+    </div>
+  );
+}
+
+export default ProductSection;
