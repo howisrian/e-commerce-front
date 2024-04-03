a9ab9ed29a28a6f32d3170539757657a5c49b436
howisrian <riansantos.dev@gmail.com>
2024-04-03 19:20:18 -0300

    Fix 7

--- my-ecommerce-app/src/components/ProductSection.js
+++ my-ecommerce-app/src/components/ProductSection.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import './../assets/styles/ProductSection.css';
 
 function ProductSection() {
   return (
-    <div className="product-section">
-      {/* Aqui você colocaria os produtos */}
-      <h2>Product Section</h2>
+    <div className="product-section-container">
+      <h2 className="section-title">Novos</h2>
+      <div className="product-section">
+        <div className="product">
+          <img src="produto1.jpg" alt="Produto 1" />
+          <div className="product-details">
+            <h3>Nome do Produto 1</h3>
+            <p>Descrição do Produto 1</p>
+            <p>R$ 50,00</p>
+          </div>
+        </div>
+        {/* Outros produtos aqui */}
+      </div>
     </div>
   );
 }
+
 
 export default ProductSection;
