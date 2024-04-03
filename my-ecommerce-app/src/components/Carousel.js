a19c694c7085c87f7a6b0e3295006db213347b38
howisrian <riansantos.dev@gmail.com>
2024-04-03 18:51:59 -0300

    Estilizando Carousel

--- my-ecommerce-app/src/components/Carousel.js
+++ my-ecommerce-app/src/components/Carousel.js
@@ -3,13 +3,18 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './../assets/styles/Carousel.css'; 
+import imgProduto1 from './../assets/img/produto1.jpg';
+import imgProduto2 from './../assets/img/produto2.jpg';
+import imgProduto3 from './../assets/img/produto3.jpg';
+import imgProduto4 from './../assets/img/produto4.jpg';
+import imgProduto5 from './../assets/img/produto5.jpg';
 
 const Carousel = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000
@@ -19,21 +24,33 @@ const Carousel = () => {
     <div className="carousel-container">
       <Slider {...settings}>
         <div className="carousel-slide">
-          <img src="./../assets/img/produto1.jpg" alt="Imagem 1" />
+          <img src={imgProduto1} alt="Imagem 1" />
           <div className="carousel-caption">
             <h3>Título da Imagem 1</h3>
           </div>
         </div>
         <div className="carousel-slide">
-          <img src="./../assets/img/produto1.jpg" alt="Imagem 2" />
+          <img src={imgProduto2} alt="Imagem 2" />
           <div className="carousel-caption">
             <h3>Título da Imagem 2</h3>
           </div>
         </div>
         <div className="carousel-slide">
-          <img src="./../assets/img/produto1.jpg" alt="Imagem 3" />
+          <img src={imgProduto3} alt="Imagem 3" />
           <div className="carousel-caption">
             <h3>Título da Imagem 3</h3>
+          </div>
+        </div>
+        <div className="carousel-slide">
+          <img src={imgProduto4} alt="Imagem 4" />
+          <div className="carousel-caption">
+            <h3>Título da Imagem 4</h3>
+          </div>
+        </div>
+        <div className="carousel-slide">
+          <img src={imgProduto5} alt="Imagem 5" />
+          <div className="carousel-caption">
+            <h3>Título da Imagem 5</h3>
           </div>
         </div>
       </Slider>
