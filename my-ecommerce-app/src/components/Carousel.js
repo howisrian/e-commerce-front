3640f92bd9532e28cd428d93c4f1d79baa892aa7
howisrian <riansantos.dev@gmail.com>
2024-04-03 18:00:40 -0300

    Adicionando Carousel

--- my-ecommerce-app/src/components/Carousel.js
+++ my-ecommerce-app/src/components/Carousel.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './../assets/styles/Carousel.css'; 
 
-function Carousel() {
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  };
+
   return (
-    <div className="carousel">
-      {/* Aqui você colocaria o conteúdo do carrossel, como imagens ou produtos em destaque */}
-      <h2>Carousel of Highlights</h2>
+    <div className="carousel-container">
+      <Slider {...settings}>
+        <div className="carousel-slide">
+          <img src="./../assets/img/produto1.jpg" alt="Imagem 1" />
+          <div className="carousel-caption">
+            <h3>Título da Imagem 1</h3>
+          </div>
+        </div>
+        <div className="carousel-slide">
+          <img src="./../assets/img/produto1.jpg" alt="Imagem 2" />
+          <div className="carousel-caption">
+            <h3>Título da Imagem 2</h3>
+          </div>
+        </div>
+        <div className="carousel-slide">
+          <img src="./../assets/img/produto1.jpg" alt="Imagem 3" />
+          <div className="carousel-caption">
+            <h3>Título da Imagem 3</h3>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 }
